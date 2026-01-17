4c71dcfa35cd1644db057fd4055cb947c365e046
Pandaniens <91369306+Pandaniens@users.noreply.github.com>
2026-01-17 20:24:49 +0100

    ModifInterface

--- Projet_IA_Scomplexes/src/UserInterface.java
+++ Projet_IA_Scomplexes/src/UserInterface.java
@@ -1,75 +1,81 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class UserInterface extends JPanel {
 
-    private int DIM_HEIGHT = 640;
-    private int DIM_WIDTH = 640;
-    private int SQ_SIZE = 40;
-    
+    private int lignes = 10;
+    private int colonnes = 10;
+    private int tailleCase = 40;
 
-    boolean black = true;
-
-    public void initialize() {
-        // Initialisation de notre fenetre
-
-        //Main frame de l'application
-        JFrame frame = new JFrame();
-        frame.setLayout(new BorderLayout());
-        
-        //Panels de dessin
-        JPanel panelDessin = new JPanel();
-        JPanel panelOutil = new JPanel();
-
-        //Panel dessin
-        panelDessin.add(this);
-        panelDessin.setPreferredSize(new Dimension(DIM_WIDTH, DIM_HEIGHT));
-
-
-        
-        
-        //Ajout des panels a la frame
-        frame.add(panelDessin, BorderLayout.CENTER);
-        frame.add(panelOutil, BorderLayout.EAST);
-
-        //Couleur des panels pour les identifier
-        panelOutil.setBackground(Color.magenta);
-        //panelDessin.paintComponents(getGraphics());
-
-        //Finalisation de la fenetre
-        //frame.add(new UserInterface());
-        frame.setVisible(true);
-        frame.setTitle("ProjectAI");
-        frame.setSize(DIM_WIDTH, DIM_HEIGHT);
-        //frame.pack();
-        frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-    }
-    public Dimension getPreferredSize() {
-        return new Dimension(DIM_WIDTH, DIM_HEIGHT);
-    }
-
-        @Override
+    @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        for (int i = 0; i < DIM_HEIGHT; i += SQ_SIZE) {
-            if (black) {
-                black = false;
-            } else {
-                black = true;
-            }
-            for (int j = 0; j < DIM_WIDTH; j += SQ_SIZE) {
-                if (black) {
-                    g.setColor(Color.WHITE);
-                    g.fillRect(j, i, SQ_SIZE, SQ_SIZE);
-                    black = false;
-                } else {
-                    g.setColor(Color.BLACK);
-                    g.fillRect(j, i, SQ_SIZE, SQ_SIZE);
-                    black = true;
-                }
+
+        ArrayList<Object> grille = createGrille();
+
+        for (int j = 0; j < grille.size(); j++) {
+            System.out.println(grille.get(j));
+        }
+
+        for (int j = 0; j < colonnes; j++) {
+            for (int i = 0; i < lignes; i++) {
+                //Couleur lier au tableau
+                if(grille.get(i*10+j).getClass() == Drone.class){
+                    g.setColor(Color.green);
+                }else
+                g.setColor((Color)grille.get(i*10+j));
+
+                g.fillRect(
+                        j * tailleCase,
+                        i * tailleCase,
+                        tailleCase,
+                        tailleCase
+                );
+
+                // Bordure noire
+                g.setColor(Color.BLACK);
+                g.drawRect(
+                        j * tailleCase,
+                        i * tailleCase,
+                        tailleCase,
+                        tailleCase
+                );
             }
         }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(
+                colonnes * tailleCase,
+                lignes * tailleCase
+        );
+    }
+
+    public ArrayList<Object> createGrille(){
+        ArrayList<Object> grilleCouleurs = new ArrayList<>();
+            for (int i = 0; i < lignes; i++) {
+                for (int j = 0; j < colonnes; j++) {
+                    grilleCouleurs.add(Color.blue);
+                }
+            }
+            //Test
+            grilleCouleurs.set(88, Color.red);
+            grilleCouleurs.set(1, new Drone());
+        return grilleCouleurs;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Grille colorée");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            frame.add(new UserInterface());
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
     }
 }
